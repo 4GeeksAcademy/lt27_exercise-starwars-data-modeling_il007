f2427dfe2313b42c511623fc8fe46bea5ab528ad
iluna007 <146373472+iluna007@users.noreply.github.com>
2024-04-13 18:39:42 +0000

    agregue relaciones entre planetas personas usuarios y favoritos

--- src/models.py
+++ src/models.py
@@ -7,26 +7,77 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class FavPeople(Base):
+    __tablename__ = 'favpeople'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    people_id = Column(Integer, ForeignKey('people.id'), primary_key=True)
+    user = relationship(User)
 
-    def to_dict(self):
-        return {}
+
+class FavPlanets(Base):
+    __tablename__ = 'favplanets'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    planets_id = Column(Integer, ForeignKey('planets.id'), primary_key=True)
+    user = relationship(User)
+
+class People(Base):
+    __tablename__ = 'people'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    birth_year = Column(String(250), nullable=False)
+    eye_color = Column(String(250), nullable=False)
+    films = Column(String(250), nullable=False)
+    gender = Column(String(250), nullable=False)
+    hair_color = Column(String(250), nullable=False)
+    height = Column(String(250), nullable=False)
+    homeworld = Column(String(250), nullable=False)
+    skin_color = Column(String(250), nullable=False)
+    created = Column(String(250), nullable=False)
+    edited = Column(String(250), nullable=False)
+    species = Column(String(250), nullable=False)
+    starships = Column(String(250), nullable=False)
+    url = Column(String(250), nullable=False)
+    vehicles = Column(String(250), nullable=False)
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    climate = Column(String(250), nullable=False)
+    created = Column(String(250), nullable=False)
+    diameter = Column(String(250), nullable=False)
+    edited = Column(String(250), nullable=False)
+    films = Column(String(250), nullable=False)
+    gravity = Column(String(250), nullable=False)
+    name = Column(String(250), nullable=False)
+    orbital_period = Column(String(250), nullable=False)
+    population = Column(String(250), nullable=False)
+    residents = Column(String(250), nullable=False)
+    rotation_period = Column(String(250), nullable=False)
+    surface_water = Column(String(250), nullable=False)
+    terrain = Column(String(250), nullable=False)
+    url = Column(String(250), nullable=False)
+
+
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
